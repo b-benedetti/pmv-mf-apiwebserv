969fba880105bec123c8d7419b36d11b4e6a6eac
Barbara Benedetti <barbarafaela2@hotmail.com>
2025-09-28 16:55:56 -0300

    update

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using mf_apiwebserv.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace mf_apiwebserv.Models
+{
+    public class AppDbContext : DbContext 
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        { 
+        }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+        public DbSet<Consumo> Consumos { get; set; }
+    }
+}
